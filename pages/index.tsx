efc94e454389c2c260dbba307fe2e1fdcb192293
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-08-09 21:08:00 -0300

    Client Side Fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,38 @@
-import { Title } from '../styles/pages/Home'
+import { Title } from '../styles/pages/Home';
+import { useState, useEffect } from 'react';
+
+interface IProduct {
+  id: string;
+  title: string;
+}
 
 export default function Home() {
+  const [recomendedProducts, setRecomendedProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended').then(response =>{
+      response.json().then(data => {
+        setRecomendedProducts(data);
+      })
+    });
+  }, []);
+  
   return (
     <div>
       <Title>Hello NextJs!</Title>
+
+      <section>
+        <Title>Products</Title>
+        <ul>
+          {recomendedProducts.map((item: IProduct) => {
+            return (
+              <li key={item.id}>
+                {item.title}
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </div>
   )
 }
