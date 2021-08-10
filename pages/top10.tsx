f159cb814db8e0580e23ba2ceabe4c507f369b0e
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-08-09 21:36:12 -0300

    Static Site Generation

---
+++ pages/top10.tsx
@@ -0,0 +1,42 @@
+import { Title } from '../styles/pages/Home';
+import { GetStaticProps } from 'next';
+
+interface IProduct {
+    id: string;
+    title: string;
+}
+
+interface Top10Props {
+    products: IProduct[];
+}
+
+export default function Top10({ products }: Top10Props) {
+    return (
+        <div>
+            <section>
+                <Title>Top 10</Title>
+                <ul>
+                    {products.map((item: IProduct) => {
+                        return (
+                            <li key={item.id}>
+                                {item.title}
+                            </li>
+                        );
+                    })}
+                </ul>
+            </section>
+        </div>
+    )
+}
+
+export const getStaticProps: GetStaticProps<Top10Props> = async (context) => {
+    const response = await fetch('http://localhost:3333/products');
+    const products = await response.json();
+
+    return {
+        props: {
+            products
+        },
+        revalidate: 10
+    }
+}
