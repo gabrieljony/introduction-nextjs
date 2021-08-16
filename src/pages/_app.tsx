f12d4e0df5086e2ed2b1fbe00ad18e2a46339f2d
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-08-16 20:55:34 -0300

    Instalação do Chakra UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
+import { ChakraProvider } from "@chakra-ui/react"
 import GlobalStyle from "@/styles/GlobalStyle";
 
 export default function MyApp({ Component, pageProps }) {
     return (
-        <>
+        <ChakraProvider>
             <GlobalStyle />
             <Component {...pageProps} />
-        </>
+        </ChakraProvider>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,17 @@ import { client } from '@/lib/prismic';
 import Prismic from 'prismic-javascript';
 import PrismicDOM from 'prismic-dom';
 import { Document } from 'prismic-javascript/types/documents';
+import { Button, ButtonGroup } from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
 
 interface IProduct {
   id: string;
@@ -16,19 +27,32 @@ interface HomeProps {
 }
 
 export default function Home({ recomendedProducts }: HomeProps) {
-  async function handleSum(){
+  async function handleSum() {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    console.log('isOpen', isOpen)
     console.log('process.env.API_UR - Variavel de ambiente restrita', process.env.API_UR)
     console.log('process.env.NEXT_PUBLIC_API_URL - Variável de ambiente publica', process.env.NEXT_PUBLIC_API_URL)
     const math = (await import('@/lib/math')).default;
-    alert(math.sum(3,5));
+    // alert(math.sum(3,5));
+    return (
+      <>
+        <Modal isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Aviso</ModalHeader>
+
+          </ModalContent>
+        </Modal>
+      </>
+    )
   }
 
   return (
     <div>
-      <SEO 
+      <SEO
         title="DevCommerce, your best ecommerce!"
         image="boost.png"
-        shouldExcludeTitleSuffix/>
+        shouldExcludeTitleSuffix />
       <Title>Hello NextJs!</Title>
 
       <section>
@@ -47,8 +71,7 @@ export default function Home({ recomendedProducts }: HomeProps) {
           })}
         </ul>
       </section>
-
-      <button onClick={handleSum}>Sum!</button>
+      <Button colorScheme="blue" onClick={handleSum}>Sum!</Button>
     </div>
   )
 }
